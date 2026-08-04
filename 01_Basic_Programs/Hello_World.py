4ca85a6c1c4fabeb1c1d044ef628dc6188602df2
Kishore0921 <kishorenbkk@gmail.com>
2026-08-04 20:22:24 +0530

    Basic Program

---
+++ 01_Basic_Programs/Hello_World.py
@@ -0,0 +1,10 @@
+"""
+Problem: Write a program to print "Hello, World!" to the console.
+Example:
+    Output: Hello, World!
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+"""
+
+print("Hello, World!")
