1e59a00d20c9756791e819d7b620f4b4bb8524ab
Kishore0921 <kishorenbkk@gmail.com>
2026-08-04 20:25:42 +0530

    Basic Program

---
+++ 01_Basic_Programs/Add_Two_numbers.py
@@ -0,0 +1,15 @@
+"""
+Problem: Write a program to add two numbers.
+Example:
+    Input: 5, 10
+    Output: 15
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+"""
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+result = a + b
+print(f"Sum: {result}")
